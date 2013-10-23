96668533c6821f0ca02a6ff598cb8ded55647a3a
FlopsKa <flops.ka@gmail.com>
2013-10-23 23:09:30 +0200

    add bin spec to scli.rb

--- features/step_definitions/scli_steps.rb
+++ features/step_definitions/scli_steps.rb
@@ -1,9 +1,12 @@
 Given(/^I run the script with add\-ssh flopska home "(.*?)"$/) do |ssh_key|
-  pending # express the regexp above with the code you wish you had
+  command = "addssh --user flopska --keyname home --key #{ssh_key}"
+  @io = StringIO.new
+  @app = Scli.new(command.split(/\s+/), @io)
 end
 
 Then(/^the key should be in "(.*?)"flopska@home\.pub$/) do |keydir|
-  pending # express the regexp above with the code you wish you had
+  # eventually add test to see if the key in the file is valid
+  File.exist?("#{keydir}/flopska@home.pub").should == true
 end
 
 Given(/^I run the script with add\-ssh flopska ubuntu "(.*?)"$/) do |ssh_key|
--- lib/scli.rb
+++ lib/scli.rb
@@ -1,4 +1,24 @@
 require 'oyster'
 
 class Scli
+  BIN_SPEC = Oyster.spec do
+    name 'scli -- suploy server command line interface'
+
+    subcommand :addssh do
+      name 'scli add'
+      string :user, :desc => 'user the key corresponds to'
+      string :keyname, :desc => 'name to identify the key'
+      string :key, :desc => 'the ssh-key'
+    end
+  end
+
+  def initialize(argv, io)
+    @options = BIN_SPEC.parse(argv)
+    puts @options
+    @stdout = io
+  end
+
+  def add_ssh_key(user, keyname, keydir, key)
+    File.open("#{keydir}/#{user}@#{keyname}.pub", 'w') { |file| file.write(key) }
+  end
 end
