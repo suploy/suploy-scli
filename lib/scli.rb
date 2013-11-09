445797bf6b50e21e0403ce65791477ca2e7494ee
FlopsKa <flops.ka@gmail.com>
2013-11-09 22:03:28 +0100

    add feature to remove ssh keys, add git push

--- lib/scli.rb
+++ lib/scli.rb
@@ -10,7 +10,14 @@ class Scli
       string :keyname, :desc => 'name to identify the key'
       string :key, :desc => 'the ssh-key'
     end
+    subcommand :rmssh do
+      name 'scli rm'
+      string :user, :desc => 'user the key corresponds to'
+      string :keyname, :desc => 'name to identify the key'
+    end
   end
+
+	attr_accessor :keydir, :git_repo_url
 
   def initialize(argv, io)
     begin
@@ -20,6 +27,7 @@ class Scli
 
     @stdout = io
     @keydir = "./"
+		@git_repo_url = "./"
     interpret
   end
 
@@ -28,11 +36,30 @@ class Scli
       user = @options[:addssh][:user]
       keyname = @options[:addssh][:keyname]
       key = @options[:addssh][:key]
-      add_ssh_key(user, keyname, @keydir, key)
+      add_ssh_key(user, keyname, key)
+		elsif @options[:rmssh]
+			user = @options[:rmssh][:user]
+			keyname = @options[:rmssh][:keyname]
+			remove_ssh_key(user, keyname)
     end
   end
 
-  def add_ssh_key(user, keyname, keydir, key)
+  def add_ssh_key(user, keyname, key)
     File.open("#{keydir}/#{user}@#{keyname}.pub", 'w') { |file| file.write(key + "\n") }
+		push_git_repo
   end
+
+	def remove_ssh_key(user, keyname)
+		File.delete("#{@keydir}/#{user}@#{keyname}.pub")
+		push_git_repo
+	end
+
+	def push_git_repo
+		# maybe use grit here?
+		Dir.chdir(@git_repo_url) do
+			system('git add .')
+			system('git commit -m "auto commit"')
+			system('git push origin master')
+		end
+	end
 end
