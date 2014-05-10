f1be3dfaf3767db9172ac4e8c8f89c47ebe33d8f
jannishuebl <jannis.huebl@web.de>
2014-05-10 20:05:15 +0200

    fix webapp backend connection and clean up

--- lib/suploy-scli.rb
+++ lib/suploy-scli.rb
@@ -1,81 +1,77 @@
 require 'fileutils'
 require 'tempfile'
-
 require 'json'
-
 require 'gitolite'
 require 'docker'
 
 
-
-require 'suploy-scli/version.rb'
+require './lib/suploy-scli/lib/suploy-scli/version.rb'
 
 class Scli
-	def initialize
-		@stdout = io
-		@keydir = "./"
-		@conffile = "./gitolite.conf"
-		@git_repo_url = "./"
-    @ga_repo = Gitolite::GitoliteAdmin.new("./")
-    @gitolite_config = ga_repo.config
-	end
+  def initialize(gitolite_dir, git_repo_dir)
+    @stdout = Kernel
+    @keydir = "#{gitolite_dir}/keydir"
+    @conffile = "#{gitolite_dir}/conf/gitolite.conf"
+    @git_repo_dir = git_repo_dir
+    @ga_repo = Gitolite::GitoliteAdmin.new(gitolite_dir)
+    @gitolite_config = @ga_repo.config
+  end
 
-	def add_ssh_key(user, keyname, key)
-		File.open("#{keydir}/#{user}@#{keyname}.pub", 'w') { |file| file.write(key + "\n") }
-		push_git_repo
-	end
+  def add_ssh_key(user, keyname, key)
+    File.open("#{@keydir}/#{user}@#{keyname}.pub", 'w') { |file| file.write(key + "\n") }
+    push_git_repo
+  end
 
-	def remove_ssh_key(user, keyname)
-		File.delete("#{@keydir}/#{user}@#{keyname}.pub")
-		push_git_repo
-	end
+  def remove_ssh_key(user, keyname)
+    File.delete("#{@keydir}/#{user}@#{keyname}.pub")
+    push_git_repo
+  end
 
-	def add_repository(user, repo)
+  def add_repository(user, repo)
     repo = Gitolite::Config::Repo.new(repo)
-    repo.add_permission(user)
+    repo.add_permission("RW+", "", user)
     @gitolite_config.add_repo(repo)
     @ga_repo.save_and_apply
-	end
+  end
 
-	def remove_repository(repo) 
-		tmp = Tempfile.new("extract")
+  def remove_repository(repo) 
+    tmp = Tempfile.new("extract")
 
-		# we have to keep deleting lines until there is an empty newline
-		line_deleted = false
+    # we have to keep deleting lines until there is an empty newline
+    line_deleted = false
 
-		open("#@conffile", 'r').each do |l|
-			if l =~	/^\s*$/ && line_deleted
-				line_deleted = false 
-				next # don't write the empty newline
-			end
-			next if line_deleted
+    open("#@conffile", 'r').each do |l|
+      if l =~	/^\s*$/ && line_deleted
+        line_deleted = false 
+        next # don't write the empty newline
+      end
+      next if line_deleted
 
-			unless l =~ /^repo\s#{repo}$/
-				tmp << l 
-			else
-				line_deleted = true
-			end
-		end
+      unless l =~ /^repo\s#{repo}$/
+        tmp << l 
+      else
+        line_deleted = true
+      end
+    end
 
-		tmp.close
-		FileUtils.mv(tmp.path, "#@conffile")
+    tmp.close
+    FileUtils.mv(tmp.path, "#@conffile")
     push_git_repo
-	end
+  end
 
-	def push_git_repo
-		# maybe use grit here?
-		return
-		Dir.chdir(@git_repo_url) do
-			system('git add --all')
-			system('git commit -m "auto commit"')
-			system('git push origin master')
-		end
-	end
+  def push_git_repo
+    # maybe use grit here?
+    Dir.chdir(@git_repo_dir) do
+      system('git add --all')
+      system('git commit -m "auto commit"')
+      system('git push origin master')
+    end
+  end
 
-	def view_container(repo) 
+  def view_container(repo) 
     container = Docker::Container.get(repo)
     container.json
-	end
+  end
 
   def start_container(repo)
     container = Docker::Container.get(repo)
