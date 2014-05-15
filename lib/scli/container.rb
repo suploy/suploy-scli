364e3aa3a8939105fded07f1593184dcae3f1d95
suploy <suploy@suploy.com>
2014-05-15 10:41:57 +0000

    fix name collison, fix object creation of ScliSSHKey

--- lib/scli/container.rb
+++ lib/scli/container.rb
@@ -1,26 +1,24 @@
-module Suploy_scli
-  class Scli
-    class Container
+module Scli
+  class Container
 
-      def view_container(repo) 
-        container = Docker::Container.get(repo)
-        container.json
-      end
-
-      def start_container(repo)
-        container = Docker::Container.get(repo)
-        result_json = container.start
-      end
-
-      def stop_container(repo)
-        container = Docker::Container.get(repo)
-        container.stop
-      end
-
-      def view_all_container()
-        Docker::Container.all(:all => true)
-      end
-
+    def view_container(repo) 
+      container = Docker::Container.get(repo)
+      container.json
     end
+
+    def start_container(repo)
+      container = Docker::Container.get(repo)
+      result_json = container.start
+    end
+
+    def stop_container(repo)
+      container = Docker::Container.get(repo)
+      container.stop
+    end
+
+    def view_all_container()
+      Docker::Container.all(:all => true)
+    end
+
   end
 end
--- lib/scli/ssh_key.rb
+++ lib/scli/ssh_key.rb
@@ -1,27 +1,27 @@
-module Suploy_scli
-  class Scli
-    class SSHKey
+module Scli
+  # Ugly name but otherwise collision with SSHKey class 
+  # from the sshkey gem
+  class ScliSSHKey
 
-      def initialize(gitolite_dir)
-        @keydir = "#{gitolite_dir}/keydir"
-        @ga_repo = Gitolite::GitoliteAdmin.new(gitolite_dir)
-      end
+    def initialize(gitolite_dir)
+      @keydir = "#{gitolite_dir}/keydir"
+      @ga_repo = Gitolite::GitoliteAdmin.new(gitolite_dir)
+    end
 
-      def add_ssh_key(user, keyname, key_string)
-        if SSHKey.valid_ssh_public_key? key_string
-          key = Gitolite::SSHKey.from_string(key_string, user, keyname)
-          @ga_repo.add_key(key)
-          @ga_repo.save_and_apply
-        else 
-          puts "Not a valid ssh key"
-        end 
-      end
-
-      def remove_ssh_key(user, keyname)
-        key = Gitolite::SSHKey.from_file("#{@keydir}/#{user}@#{keyname}.pub")
-        @ga_repo.rm_key(key)
+    def add_ssh_key(user, keyname, key_string)
+      if SSHKey.valid_ssh_public_key? key_string
+        key = Gitolite::SSHKey.from_string(key_string, user, keyname)
+        @ga_repo.add_key(key)
         @ga_repo.save_and_apply
-      end
+      else 
+        puts "Not a valid ssh key"
+      end 
+    end
+
+    def remove_ssh_key(user, keyname)
+      key = Gitolite::SSHKey.from_file("#{@keydir}/#{user}@#{keyname}.pub")
+      @ga_repo.rm_key(key)
+      @ga_repo.save_and_apply
     end
   end
 end
