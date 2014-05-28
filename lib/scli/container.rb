4ec4e94b930f30769b90f53f786c45bb6d472322
flopska <flops.ka@gmail.com>
2014-05-28 21:47:44 +0200

    add linking functionality

--- lib/scli/container.rb
+++ lib/scli/container.rb
@@ -11,6 +11,19 @@ module Scli
       result_json = container.start
     end
 
+		# Creates a new docker container from the image which is connected to the
+		# container 'db_container_name'. Use this for database access
+		def create_app_container_with_db(image_name, environment, db_container_name)
+			container = Docker::Container.create('Cmd' => ['/bin/bash', '-c', '/start web'],
+															 'Image' => image_name,
+															 'Env' => 'PORT=5000',
+															 'ExposedPorts' => {
+																 "5000/tcp" => {}
+															 })
+			params = { "Links" => ["#{db_container_name}:db"] }
+			container.start(params);
+		end
+
     def stop_container(repo)
       container = Docker::Container.get(repo)
       container.stop
